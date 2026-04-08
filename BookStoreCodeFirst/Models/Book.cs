fb849f89418054ee7fb14b3e38d3dbb162c70524
NguyenMTue <23115053122342@sv.ute.udn.vn>
2026-04-08 21:23:49 +0700

    create modelclass

---
+++ BookStoreCodeFirst/Models/Book.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace BookStoreCodeFirst.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public decimal Price { get; set; }
+    }
+}
